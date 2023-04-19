c2717eaa58fec71a4e9ffde2cab637d8cffc3faa
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-19 17:21:05 +0300

    3 Completed logic of data exchange between two files

--- HW28_C++base_r00_20230418.cpp
+++ HW28_C++base_r00_20230418.cpp
@@ -1,6 +1,7 @@
-#include<fstream>
 #include <iostream>
 #include <string>
+#include<fstream>
+#include<stdio.h>
 
 using namespace std;
 
@@ -33,39 +34,163 @@ void Data(Man* man)
 	man[5] = { "Borodina",		"Ganna",		21, "2002.05.08", "BR211", "Devop" };
 }
 
-int main()
+void fileList(Man man[], string  arrfname[])
 {
-	Data(man);
-	string arrfname[row]{};
 	string fname = "file_";
-	
-	cout << "\n List of files:\n";
+
+	cout << "\n  List of files:\n";
 	for (int i = 0, j = 1; i < row; i++, j++)
 	{
 		string n{};
 		n = to_string(j);
 		arrfname[i] = fname + n + ".txt";
-		cout << "\t" << arrfname[i] << endl;
+		cout << " | " << i + 1 << " | " << arrfname[i] << " |\n";
 	}
 
 	for (int i = 0; i < row; i++)
 	{
-		ofstream out;
-		out.open(arrfname[i]);
-		if (!out)
+		ofstream fout;
+		fout.open(arrfname[i]);
+		if (!fout)
 		{
 			cout << "\n\tCannot open the file " << arrfname[i];
 			continue;
 		}
 
-		out
+		fout
 			<< man[i].sname << " " << man[i].name << endl
 			<< man[i].age << " yers old" << endl
 			<< man[i].date << endl
 			<< man[i].group << endl
 			<< man[i].direct;
 
-		out.close();
+		fout.close();
+	}
+}
+
+int main()
+{
+	Data(man);
+	string arrfname[row]{};
+	fileList(man, arrfname);
+
+	//Menu
+restart1:
+	int qty = 0;
+	cout << "\n Qty of files for exchange: ";
+	cin >> qty;
+
+	if (qty > row || qty % 2 != 0)
+	{
+		cout << "\n ERROR. incorrect input";
+		goto restart1;
+	}
+
+	int* arr = new int[qty];
+	int i = 0, j = 0;
+
+
+	while (i < qty)
+	{
+		cout << "\n File package N" << j + 1;
+		cout << "\n\t1-st file: Num -> ";
+		cin >> arr[(i++)];
+		cout << "\t2-nd file: Num -> ";
+		cin >> arr[(i++)];
+		j++;
+	}
+
+
+	//Function of exchange data between Files
+	
+	int n = 0;
+	while (n < qty)
+	{
+		int x = 0, y = 0;
+		x = arr[n++];
+		y = arr[n++];
+		x--, y--;
+
+		//move data File X -> File temp
+		ofstream fout_t;
+		fout_t.open("file_t.txt", ios::trunc);
+		fout_t.close();
+
+		fout_t.open("file_t.txt", ios::app);
+
+		ifstream fin_x;
+		fin_x.open(arrfname[x]);
+
+		if (!fin_x.is_open() || !fout_t.is_open())
+		{
+			cout << "\n\tCannot open the file";
+		}
+		else
+		{
+			while (!fin_x.eof())
+			{
+				string str{};
+				getline(fin_x, str);
+				fout_t << str << "\n";
+			}
+		}
+		fin_x.close();
+		fout_t.close();
+
+		//move data File Y -> File Y
+		ifstream fin_y;
+		fin_y.open(arrfname[y]);
+
+		ofstream fout_x;
+		fout_x.open(arrfname[x], ios::out);
+		fout_x.close();
+
+		fout_x.open(arrfname[x], ios::app);
+
+		if (!fin_y.is_open() || !fout_x.is_open())
+		{
+			cout << "\n\tCannot open the file";
+		}
+		else
+		{
+			while (!fin_y.eof())
+			{
+				string str{};
+				getline(fin_y, str);
+				fout_x << str << "\n";
+			}
+		}
+		fin_y.close();
+		fout_x.close();
+
+		//move data File temp -> File Y
+		ifstream fin_t;
+		fin_t.open("file_t.txt");
+
+		ofstream fout_y;
+		fout_y.open(arrfname[y], ios::out);
+		fout_y.close();
+
+		fout_y.open(arrfname[y], ios::app);
+
+		if (!fin_t.is_open() || !fout_y.is_open())
+		{
+			cout << "\n\tCannot open the file";
+		}
+		else
+		{
+			while (!fin_t.eof())
+			{
+				string str{};
+				getline(fin_t, str);
+				fout_y << str << "\n";
+			}
+		}
+		fin_t.close();
+		fin_t.open("file_t.txt");
+		fout_y.close();
+
+		remove("file_t.txt");
 	}
 
 	return 0;
